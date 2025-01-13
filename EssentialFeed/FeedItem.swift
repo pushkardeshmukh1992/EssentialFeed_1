4f772cb46c2b9643f7fe6e1091182cf4209675ea
Pushkar Deshmukh <pushkard@backbase.com>
2025-01-13 23:11:23 +0530

    Add FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Pushkar Deshmukh on 13/01/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
